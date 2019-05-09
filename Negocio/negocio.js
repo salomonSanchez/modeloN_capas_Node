b239436697dd6010efedb2ca1836189276ebbdba
Manuel Rojas <m.rojas@unas.edu.pe>
2019-05-08 21:44:57 -0500

    aaa

---
+++ Negocio/negocio.js
@@ -0,0 +1,23 @@
+const controller = {}
+const mysql = require('mysql')
+const url =require('../routes/database')
+const dbconnection = mysql.createConnection(url.urlMysql)
+
+
+controller.list  = (req, res) => {
+  dbconnection.query("SELECT * FROM revist", (error, results, fields)=>{
+
+    res.render("Revista",{
+        data: results
+    })
+});
+}
+
+controller.agregar = (req, res) =>{
+    const data=req.body
+   dbconnection.query('INSERT INTO revist set ?',[data], (err,results)=>{
+       res.redirect('/')
+   })
+}
+
+module.exports = controller
---
+++ baseDeDatos/data.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+     
+    //Please replace your host file Here : 127.1.1.0 , Express is Collection Name (Database Name)
+    'urlMysql':{
+            host:'localhost',
+            user:'root',
+            password:'',
+            database:'revista'
+           
+    }
+
+
+};
